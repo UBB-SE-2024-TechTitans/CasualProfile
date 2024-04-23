c5964d4658ed4be49aaf68d382e0460da428a1d7
912-Eftimie-Eduard <eduard.eftimie@stud.ubbcluj.ro>
2024-04-23 23:23:54 +0300

    ChangedTheStructure

--- District 3 App Tests/ServiceTests/ProfileNetworkService_Dianacs.cs
+++ District 3 App Tests/ServiceTests/ProfileNetworkService_Dianacs.cs
@@ -7,12 +7,11 @@ namespace District_3_App_Tests.ServiceTests
 {
     public class ProfileNetworkInfoServiceTests
     {
-        [Fact]
-        public void AddProfileSocialNetworkInfo_NewValidProfile_ShouldReturnTrue()
-        {
+        ProfileNetworkInfoService service;
+        UserProfileSocialNetworkInfo profile;
 
-            ///
-            // Create mock users
+        private void Initialize()
+        {
             User user1 = new User(Guid.NewGuid(), "user1", "password1", "user1@example.com", "password1Confirmation");
             User user2 = new User(Guid.NewGuid(), "user2", "password2", "user2@example.com", "password2Confirmation");
             User user3 = new User(Guid.NewGuid(), "user3", "password3", "user3@example.com", "password3Confirmation");
@@ -51,15 +50,19 @@ namespace District_3_App_Tests.ServiceTests
 
             GroupsRepository groupsRepository = new GroupsRepository(groups);
             List<UserProfileSocialNetworkInfo> profiles = new List<UserProfileSocialNetworkInfo> { profile1, profile2, profile3 };
-            
+
             ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo> profileNetworkInfoRepository = new ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo>(profiles);
             UsersRepository usersRepository = new UsersRepository(new List<User> { user1, user2, user3, user4 });
-            // Arrange
-            var service = new ProfileNetworkInfoService(groupsRepository, profileNetworkInfoRepository, usersRepository);
-            var profile = new UserProfileSocialNetworkInfo();
+            this.service = new ProfileNetworkInfoService(groupsRepository, profileNetworkInfoRepository, usersRepository);
+            this.profile = new UserProfileSocialNetworkInfo();
+        }
 
+        [Fact]
+        public void AddProfileSocialNetworkInfo_NewValidProfile_ShouldReturnTrue()
+        {
+            this.Initialize();
             // Act
-            var result = service.AddProfileSocialNetworkInfo(profile);
+            var result = this.service.AddProfileSocialNetworkInfo(this.profile);
 
             // Assert
             Assert.True(result);
@@ -68,176 +71,50 @@ namespace District_3_App_Tests.ServiceTests
         [Fact]
         public void AddProfileSocialNetworkInfo_ProfileAlreadyInDatabase_ShouldReturnFalse()
         {
-            // Create mock users
-            User user1 = new User(Guid.NewGuid(), "user1", "password1", "user1@example.com", "password1Confirmation");
-            User user2 = new User(Guid.NewGuid(), "user2", "password2", "user2@example.com", "password2Confirmation");
-            User user3 = new User(Guid.NewGuid(), "user3", "password3", "user3@example.com", "password3Confirmation");
-            User user4 = new User(Guid.NewGuid(), "black_ship", "password4", "user4@example.com", "password4Confirmation");
-
-            // Create mock groups
-            List<User> groupMembers1 = new List<User> { user1, user2 };
-            List<User> groupMembers2 = new List<User> { user2, user3 };
-
-            Group group1 = new Group(Guid.NewGuid(), "Group 1", groupMembers1);
-            Group group2 = new Group(Guid.NewGuid(), "Group 2", groupMembers2);
-
-            List<Group> groups = new List<Group> { group1, group2 };
-
-            // Create mock profile social network info
-            List<BlockedProfile> blockedProfiles1 = new List<BlockedProfile>();
-            blockedProfiles1.Add(new BlockedProfile(user4, DateTime.Now));
-            List<BlockedProfile> blockedProfiles2 = new List<BlockedProfile>();
-            List<BlockedProfile> blockedProfiles3 = new List<BlockedProfile>();
-
-            List<CloseFriendProfile> closeFriends1 = new List<CloseFriendProfile>();
-            List<CloseFriendProfile> closeFriends2 = new List<CloseFriendProfile>();
-            List<CloseFriendProfile> closeFriends3 = new List<CloseFriendProfile>();
-
-            List<User> restrictedStoriesAudience1 = new List<User>();
-            List<User> restrictedStoriesAudience2 = new List<User>();
-            List<User> restrictedStoriesAudience3 = new List<User>();
-
-            List<User> restrictedPostsAudience1 = new List<User>();
-            List<User> restrictedPostsAudience2 = new List<User>();
-            List<User> restrictedPostsAudience3 = new List<User>();
-
-            UserProfileSocialNetworkInfo profile1 = new UserProfileSocialNetworkInfo(user1, blockedProfiles1, closeFriends1, new List<Group> { group1 }, restrictedStoriesAudience1, restrictedPostsAudience1);
-            UserProfileSocialNetworkInfo profile2 = new UserProfileSocialNetworkInfo(user2, blockedProfiles2, closeFriends2, new List<Group> { group2 }, restrictedStoriesAudience2, restrictedPostsAudience2);
-            UserProfileSocialNetworkInfo profile3 = new UserProfileSocialNetworkInfo(user3, blockedProfiles3, closeFriends3, new List<Group>(), restrictedStoriesAudience3, restrictedPostsAudience3);
-
-            GroupsRepository groupsRepository = new GroupsRepository(groups);
-            List<UserProfileSocialNetworkInfo> profiles = new List<UserProfileSocialNetworkInfo> { profile1, profile2, profile3 };
-
-            // Add a profile that already exists
-            profiles.Add(profile1);
-
-            ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo> profileNetworkInfoRepository = new ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo>(profiles);
-            UsersRepository usersRepository = new UsersRepository(new List<User> { user1, user2, user3, user4 });
-
+            this.Initialize();
             // Arrange
-            var service = new ProfileNetworkInfoService(groupsRepository, profileNetworkInfoRepository, usersRepository);
-            var profile = new UserProfileSocialNetworkInfo(user1, blockedProfiles1, closeFriends1, new List<Group> { group1 }, restrictedStoriesAudience1, restrictedPostsAudience1);
+            this.service.AddProfileSocialNetworkInfo(this.profile);
 
             // Act
-            var result = service.AddProfileSocialNetworkInfo(profile);
+            var result = this.service.AddProfileSocialNetworkInfo(this.profile);
 
             // Assert
             Assert.False(result);
         }
+
+        [Fact]
+        public void CheckIfProfileExists_ProfileExists_ShouldReturnTrue()
+        {
+            this.Initialize();
+            this.service.AddProfileSocialNetworkInfo(this.profile);
+            var current_answer = this.service.CheckIfProfileExists(this.profile);
+            var expected_answer = true;
+            Assert.Equal(expected_answer, current_answer);
+        }
+
         [Fact]
         public void RemoveProfileSocialNetworkInfo_ShouldReturnTrue()
         {
-            // Create mock users
-            User user1 = new User(Guid.NewGuid(), "user1", "password1", "user1@example.com", "password1Confirmation");
-            User user2 = new User(Guid.NewGuid(), "user2", "password2", "user2@example.com", "password2Confirmation");
-            User user3 = new User(Guid.NewGuid(), "user3", "password3", "user3@example.com", "password3Confirmation");
-            User user4 = new User(Guid.NewGuid(), "black_ship", "password4", "user4@example.com", "password4Confirmation");
-
-            // Create mock groups
-            List<User> groupMembers1 = new List<User> { user1, user2 };
-            List<User> groupMembers2 = new List<User> { user2, user3 };
-
-            Group group1 = new Group(Guid.NewGuid(), "Group 1", groupMembers1);
-            Group group2 = new Group(Guid.NewGuid(), "Group 2", groupMembers2);
-
-            List<Group> groups = new List<Group> { group1, group2 };
-
-            // Create mock profile social network info
-            List<BlockedProfile> blockedProfiles1 = new List<BlockedProfile>();
-            blockedProfiles1.Add(new BlockedProfile(user4, DateTime.Now));
-            List<BlockedProfile> blockedProfiles2 = new List<BlockedProfile>();
-            List<BlockedProfile> blockedProfiles3 = new List<BlockedProfile>();
-
-            List<CloseFriendProfile> closeFriends1 = new List<CloseFriendProfile>();
-            List<CloseFriendProfile> closeFriends2 = new List<CloseFriendProfile>();
-            List<CloseFriendProfile> closeFriends3 = new List<CloseFriendProfile>();
-
-            List<User> restrictedStoriesAudience1 = new List<User>();
-            List<User> restrictedStoriesAudience2 = new List<User>();
-            List<User> restrictedStoriesAudience3 = new List<User>();
-
-            List<User> restrictedPostsAudience1 = new List<User>();
-            List<User> restrictedPostsAudience2 = new List<User>();
-            List<User> restrictedPostsAudience3 = new List<User>();
-
-            UserProfileSocialNetworkInfo profile1 = new UserProfileSocialNetworkInfo(user1, blockedProfiles1, closeFriends1, new List<Group> { group1 }, restrictedStoriesAudience1, restrictedPostsAudience1);
-            UserProfileSocialNetworkInfo profile2 = new UserProfileSocialNetworkInfo(user2, blockedProfiles2, closeFriends2, new List<Group> { group2 }, restrictedStoriesAudience2, restrictedPostsAudience2);
-            UserProfileSocialNetworkInfo profile3 = new UserProfileSocialNetworkInfo(user3, blockedProfiles3, closeFriends3, new List<Group>(), restrictedStoriesAudience3, restrictedPostsAudience3);
-
-            GroupsRepository groupsRepository = new GroupsRepository(groups);
-            List<UserProfileSocialNetworkInfo> profiles = new List<UserProfileSocialNetworkInfo> { profile1, profile2, profile3 };
-
-            ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo> profileNetworkInfoRepository = new ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo>(profiles);
-            UsersRepository usersRepository = new UsersRepository(new List<User> { user1, user2, user3, user4 });
-
-            // Arrange
-            var service = new ProfileNetworkInfoService(groupsRepository, profileNetworkInfoRepository, usersRepository);
-            var profileToRemove = new UserProfileSocialNetworkInfo();
-
+            this.Initialize();
             // First, add the profile to the service
-            service.AddProfileSocialNetworkInfo(profileToRemove);
+            this.service.AddProfileSocialNetworkInfo(this.profile);
 
             // Act
-            var result = service.RemoveProfileSocialNetworkInfo(profileToRemove);
+            var result = service.RemoveProfileSocialNetworkInfo(profile);
+            var expected_answer = true;
 
             // Assert
-            Assert.True(result);
+            Assert.Equal(expected_answer, result);
         }
         [Fact]
         public void RemoveProfileSocialNetworkInfo_ShouldReturnFalse()
         {
-            // Create mock users
-            User user1 = new User(Guid.NewGuid(), "user1", "password1", "user1@example.com", "password1Confirmation");
-            User user2 = new User(Guid.NewGuid(), "user2", "password2", "user2@example.com", "password2Confirmation");
-            User user3 = new User(Guid.NewGuid(), "user3", "password3", "user3@example.com", "password3Confirmation");
-            User user4 = new User(Guid.NewGuid(), "black_ship", "password4", "user4@example.com", "password4Confirmation");
-
-            // Create mock groups
-            List<User> groupMembers1 = new List<User> { user1, user2 };
-            List<User> groupMembers2 = new List<User> { user2, user3 };
-
-            Group group1 = new Group(Guid.NewGuid(), "Group 1", groupMembers1);
-            Group group2 = new Group(Guid.NewGuid(), "Group 2", groupMembers2);
-
-            List<Group> groups = new List<Group> { group1, group2 };
-
-            // Create mock profile social network info
-            List<BlockedProfile> blockedProfiles1 = new List<BlockedProfile>();
-            blockedProfiles1.Add(new BlockedProfile(user4, DateTime.Now));
-            List<BlockedProfile> blockedProfiles2 = new List<BlockedProfile>();
-            List<BlockedProfile> blockedProfiles3 = new List<BlockedProfile>();
-
-            List<CloseFriendProfile> closeFriends1 = new List<CloseFriendProfile>();
-            List<CloseFriendProfile> closeFriends2 = new List<CloseFriendProfile>();
-            List<CloseFriendProfile> closeFriends3 = new List<CloseFriendProfile>();
-
-            List<User> restrictedStoriesAudience1 = new List<User>();
-            List<User> restrictedStoriesAudience2 = new List<User>();
-            List<User> restrictedStoriesAudience3 = new List<User>();
-
-            List<User> restrictedPostsAudience1 = new List<User>();
-            List<User> restrictedPostsAudience2 = new List<User>();
-            List<User> restrictedPostsAudience3 = new List<User>();
-
-            UserProfileSocialNetworkInfo profile1 = new UserProfileSocialNetworkInfo(user1, blockedProfiles1, closeFriends1, new List<Group> { group1 }, restrictedStoriesAudience1, restrictedPostsAudience1);
-            UserProfileSocialNetworkInfo profile2 = new UserProfileSocialNetworkInfo(user2, blockedProfiles2, closeFriends2, new List<Group> { group2 }, restrictedStoriesAudience2, restrictedPostsAudience2);
-            UserProfileSocialNetworkInfo profile3 = new UserProfileSocialNetworkInfo(user3, blockedProfiles3, closeFriends3, new List<Group>(), restrictedStoriesAudience3, restrictedPostsAudience3);
-
-            GroupsRepository groupsRepository = new GroupsRepository(groups);
-            List<UserProfileSocialNetworkInfo> profiles = new List<UserProfileSocialNetworkInfo> { profile1, profile2, profile3 };
-
-            ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo> profileNetworkInfoRepository = new ProfileNetworkInfoRepository<UserProfileSocialNetworkInfo>(profiles);
-            UsersRepository usersRepository = new UsersRepository(new List<User> { user1, user2, user3, user4 });
-
-            // Arrange
-            var service = new ProfileNetworkInfoService(groupsRepository, profileNetworkInfoRepository, usersRepository);
-            var profileToRemove = new UserProfileSocialNetworkInfo(); // A profile that doesn't exist in the repository
-
+            this.Initialize();
             // Act
-            var result = service.RemoveProfileSocialNetworkInfo(profileToRemove);
-
+            var result = service.RemoveProfileSocialNetworkInfo(profile);
+            var expected_answer = false;
             // Assert
-            Assert.False(result);
+            Assert.Equal(expected_answer, result);
         }
         [Fact]
         public void AddGroupToCurrentUser_ProfileExistsAndGroupNotAdded_ShouldReturnTrue()
